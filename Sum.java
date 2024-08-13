68cee332a491a4d60c16c4d69a99fd9ae26efcfa
LovMahura <lov.mahura_cs23@gla.ac.in>
2024-08-13 10:06:23 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+     public static void main(String[] args) {
+     if (args.length >= 2){String s1 = args[0];
+     String s2 = args[1];
+     int num1 = Integer.valueOf(s1);
+     int num2 = Integer.valueOf(s2);
+     System.out.println("The sum of " + num1 + "and " + num2 + " is: " + (num1+num2));
+          }
+     }
+} 
---
+++ Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome {
+     public static void main(String[] args) {
+        String str1 = args[0];
+        String str2 = args[1];
+        String str3 = args[-1];
+        System.out.print("Welcome " + str3);
+     }
+}
